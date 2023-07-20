2fd204457fb4a17fd42e68ba2230f448690aae57
Adam Cin <adamcin0929@outlook.com>
2023-07-20 17:46:01 +0800

    將舊moviexpress轉移

--- src/App.vue
+++ src/App.vue
@@ -4,28 +4,69 @@
     <router-link to="/about">About</router-link>|
     <router-link to="/setting">Setting</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import "~bootstrap/scss/bootstrap";
+
+html {
+  height: 100%;
 }
 
-nav {
-  padding: 30px;
+body {
+  font-family: 思源黑體, 微軟正黑體, 蘋方黑體, 華康麗黑體, Helvetica, Arial, sans-serif, serif;
+  margin: 0;
+  padding: 0px;
+  background-color: black;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+#header {
+  height: 72px;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+@media screen and (max-width:1280px) {
+  .navbar {
+    padding: 0px !important;
   }
+}
+
+button:focus {
+  box-shadow: none !important;
+}
+
+.navbar {
+  padding: 0px 320px;
+  background-color: #131b3b;
+}
+
+.nav-link {
+  color: white !important;
+  transition: all 0.05s ease-in-out 0.2s !important;
+  font-size: 1rem;
+}
+
+.nav-link:hover {
+  text-shadow: 0.1em 0.1em 0.2em white;
+}
+
+.container-fluid {
+  padding: 6px;
+}
+
+
+.d-flex input {
+  width: 260px;
+}
+
+.d-flex input:focus {
+  box-shadow: 0 0 3px 1px white !important;
+}
+
+.d-flex button {
+  background-image: url('@/assets/img/search_icon.png');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BannerSection/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import BannerSection from '@/components/banner.vue';
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+    name: 'banner',
+    components: {     BannerSection   }
 }
 </script>
